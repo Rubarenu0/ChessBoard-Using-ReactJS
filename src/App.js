cec30c8bc5430bee864ba2d75e07f3b33b0c33ef
Rubarenu0 <rubaknr@gmail.com>
2024-08-27 18:59:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr className='row1'>
+            <td class="white" align="center">♜</td>
+            <td class="black" align="center">♞</td>
+            <td class="white" align="center">♝</td>
+            <td class="black" align="center">♛</td>
+            <td class="white" align="center">♚</td>
+            <td class="black" align="center">♝</td>
+            <td class="white" align="center">♞</td>
+            <td class="black" align="center">♜</td>
+
+        </tr>
+        <tr className='row2'>
+            <td class="black b-pawn" align="center"> ♟</td>
+            <td class="white b-pawn"  align="center"> ♟</td>
+            <td class="black b-pawn"  align="center"> ♟</td>
+            <td class="white b-pawn"  align="center"> ♟</td>
+            <td class="black b-pawn"  align="center"> ♟</td>
+            <td class="white b-pawn"  align="center"> ♟</td>
+            <td class="black b-pawn"  align="center"> ♟</td>
+            <td class="white b-pawn"  align="center"> ♟</td>
+        </tr>
+       
+        <tr>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+
+        </tr>
+        <tr>
+           
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            
+        </tr>
+        <tr>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+        </tr>
+        <tr>
+           
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+            <td class="black"></td>
+            <td class="white"></td>
+        </tr>
+        <tr className='row7'>
+            <td class="white w-pawn"  align="center">♙</td>
+            <td class="black w-pawn"  align="center">♙</td>
+            <td class="white w-pawn"  align="center">♙</td>
+            <td class="black w-pawn"  align="center">♙</td>
+            <td class="white w-pawn"  align="center">♙</td>
+            <td class="black w-pawn"  align="center">♙</td>
+            <td class="white w-pawn"  align="center">♙</td>
+            <td class="black w-pawn"  align="center">♙</td>
+        </tr>
+        <tr className='row8'>
+            <td class="black"  align="center">♖</td>
+            <td class="white"  align="center">♘</td>
+            <td class="black"  align="center">♗</td>
+            <td class="white"  align="center">♕</td>
+            <td class="black"  align="center">♔</td>
+            <td class="white"  align="center">♗</td>
+            <td class="black"  align="center">♘</td>
+            <td class="white"  align="center">♖</td>
+        </tr>
+       
+    </table>
+    </div>
+  );
+}
+
+export default App;
